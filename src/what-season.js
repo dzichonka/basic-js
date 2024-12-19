57e3733207f132f863c24ab4bb6c4c3b31b7e794
Anna Vasilevich <dzichonka@gmail.com>
2024-12-20 00:02:30 +0100

    feat: passed 62/83

--- src/what-season.js
+++ src/what-season.js
@@ -2,24 +2,28 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
 function getSeason(date) {
   if (!date) {
     return 'Unable to determine the time of year!';
   }
-  if (!(date instanceof Date) || isNaN(date.getTime())) {
+  try {
+    date.getTime();
+  } catch {
+    throw new Error('Invalid date!');
+  }
+  if (!(date instanceof Date) || Object.prototype.toString.call(date) !== '[object Date]') {
     throw new Error('Invalid date!');
   }
   const month = date.getMonth();
-
   if (month >= 2 && month <= 4) {
     return 'spring';
   } else if (month >= 5 && month <= 7) {
